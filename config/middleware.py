f5ae1dc9c879d040b620873d46daff1f15cb6ce5
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2022-09-25 00:00:50 +0900

    translate

--- config/middleware.py
+++ config/middleware.py
@@ -1,5 +1,11 @@
+from social_core.exceptions import AuthCanceled
 from social_django.middleware import SocialAuthExceptionMiddleware
 
 
 class CustomizedSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
-    pass
+    def get_message(self, request, exception):
+        if isinstance(exception, AuthCanceled):
+            # TODO: 本当はここで適切なエラーメッセージを表示したい
+            pass
+
+        return f"ログインに失敗しました（例外: {exception.__class__.__name__}）"
